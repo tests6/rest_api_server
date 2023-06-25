7c944794cef04aef08148fee6aec951fb4cf842e
vegandiet705 <vegandiet705@gmail.com>
2023-06-26 01:11:48 +0200

    [FIX] float operations

--- calculator.py
+++ calculator.py
@@ -1,9 +1,19 @@
+def is_float(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+
 def evaluate_rpn(expression: list[str]) -> float:
     stack = []
 
     for token in expression:
         if token.isdigit():
             stack.append(int(token))
+        elif is_float(token):
+            stack.append(float(token))
         else:
             operand2 = stack.pop()
             operand1 = stack.pop()
